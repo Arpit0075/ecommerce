bf33b02366a315c0907291cd437a8d11856da98e
Arpit <arpitworks4@gmail.com>
2021-11-29 18:25:35 +0530

    updated authContext

--- frontend/src/Context/AuthContext.js
+++ frontend/src/Context/AuthContext.js
@@ -1,9 +1,14 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 export const Token = createContext();
 
 export const AuthContext = ({ children }) => {
   const [auth, setAuth] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) setAuth(true);
+  }, []);
 
   return <Token.Provider value={[auth, setAuth]}> {children} </Token.Provider>;
 };
