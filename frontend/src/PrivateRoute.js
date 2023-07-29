fe87974557ec5620b7615b91433ec317121b30ce
Arpit <arpitworks4@gmail.com>
2023-07-29 11:54:02 +0530

    updated private route

--- frontend/src/PrivateRoute.js
+++ frontend/src/PrivateRoute.js
@@ -1,9 +1,19 @@
 import { Navigate } from "react-router-dom";
+import jwt_decode from "jwt-decode";
 
 function PrivateRoute({ children }) {
   const token = localStorage.getItem("token");
-  //console.log(token);
-  return token ? children : <Navigate to="/login" />;
+  if (!token) {
+    return <Navigate to="/" />;
+  }
+  let decoded = jwt_decode(token);
+  let currentTime = new Date().getTime() / 1000; //in seconds
+
+  if (currentTime > decoded.exp) {
+    localStorage.removeItem("token");
+    return <Navigate to="/" />;
+  }
+  return children;
 }
 
 export default PrivateRoute;
